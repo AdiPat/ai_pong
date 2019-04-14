9b872a439060ec7ddf7866b19e8a453445213a70
Adi_Pat <adipat247360@gmail.com>
2019-04-14 12:16:22 +0530

    Implement Collision Detection

--- src/js/game/Game.js
+++ src/js/game/Game.js
@@ -1,7 +1,8 @@
+import Paddle from "../components/Paddle";
+
 export default class Game {
 
     /**
-     * 
      * @param {string} name - The name of the game 
      * @param {CanvasRenderingContext2D} ctx - Canvas Context object for rendering
      * @param {Object} objects - Game components to be drawn
@@ -47,15 +48,41 @@ export default class Game {
      * TODO: Checks for collisions and makes corrections
      */
     detectCollisions() {
-        const keys = Object.keys(this.objects);
+        const ball = this.objects['ball'];
+        const p0 = this.objects['paddle-0'];
+        const p1 = this.objects['paddle-1'];
 
-        for (var k in keys) {
-            let curObj = this.objects[k];
-            // get bounds
-            for (var kj in keys) {
-                let nextObj = this.objects[kj];
-                // check if both are not touching
+        
+        // TODO: Dirty, but works for now
+        if((ball.x + ball.radius >= 800) || (ball.x - ball.radius <= 0)) {
+            if((ball.y > p0.y && ball.y < p0.y + p0.height) || (ball.y > p1.y && ball.y < p1.y + p1.height))
+                ball.setSpeed(-ball.speedX, ball.speedY);
+            else {
+                // relaunch ball
+                ball.setLoc(240, 400);
+                ball.setSpeed(-3, 3);
             }
+        }
+    
+        // ball and vertical bounds
+        if((ball.y + ball.radius >= 480) || (ball.y - ball.radius <= 0))
+            ball.setSpeed(ball.speedX, -ball.speedY);
+
+
+        // paddles
+        if(p0.y + p0.height >= 480) {
+            p0.setLoc(0,480 - p0.height);
+        } 
+        if(p0.y - p0.height <= 0) {
+            p0.setLoc(0,0);
+        }
+
+        if(p1.y + p1.height >= 480) {
+            p1.setLoc(800 - p1.width,480 - p1.height);
+        } 
+
+        if(p1.y - p1.height <= 0) {
+            p1.setLoc(800 - p1.width,0);
         }
     }
 
