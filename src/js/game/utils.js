67228e3ba9de3062909199300bf9bf03bf05364f
Adi_Pat <adipat247360@gmail.com>
2019-04-16 00:17:17 +0530

    Lock audio resource to prevent multiple sounds

--- src/js/game/utils.js
+++ src/js/game/utils.js
@@ -4,9 +4,15 @@
  * This function requires some kind of user interaction to happen prior to calling it
  */
 
+var audio_lock = false;
+
 export function playSound(path) {
-    const aud = new Audio(path);
-    aud.play();
+    if(!audio_lock) {
+        audio_lock = true;
+        const aud = new Audio(path);
+        aud.play();
+        setTimeout(() => { audio_lock = false }, 500);
+    }   
 }
 
 export function randomInt(a, b) {
